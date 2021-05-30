aadb660661ff347acbfd4bbddfc3cd186d754fce
Kevin Jacob <kevinjacob2001@gmail.com>
2021-05-30 10:06:54 +0530

    Replaced NavLink with Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 //CSS
 import styles from "./styles/Navbar.module.css";
@@ -12,18 +12,18 @@ const Navbar = () => {
                     <h2>Dockets</h2>
                 </div>
                 <ul>
-                    <NavLink to="/" activeStyle={{ color: "white" }}>
+                    <Link to="/" activeStyle={{ color: "white" }}>
                         Home
-          </NavLink>
-                    <NavLink href="" activeStyle={{ color: "white" }}>
+          </Link>
+                    <Link href="" activeStyle={{ color: "white" }}>
                         Documentation
-          </NavLink>
-                    <NavLink href="" activeStyle={{ color: "white" }}>
+          </Link>
+                    <Link href="" activeStyle={{ color: "white" }}>
                         Learn More
-          </NavLink>
-                    <NavLink to="/Aboutus" activeStyle={{ color: "white" }}>
+          </Link>
+                    <Link to="/Aboutus" activeStyle={{ color: "white" }}>
                         About Us
-          </NavLink>
+          </Link>
                 </ul>
             </div>
         </nav>
